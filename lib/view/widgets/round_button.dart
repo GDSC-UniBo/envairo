8ee8322ae0f32562ce5cf8a1442f86c675543d8f
Omar <omar.younis98@gmail.com>
2022-02-24 22:45:16 +0100

    add round button

---
+++ lib/view/widgets/round_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RoundButton extends StatelessWidget {
+  final Widget child;
+  final Color buttonColor;
+  final void Function() onTap;
+
+  const RoundButton({
+    Key? key,
+    required this.child,
+    required this.buttonColor,
+    required this.onTap
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10.0.h, horizontal: 30.w),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1.sh),
+              color: buttonColor
+          ),
+          child: child
+        ),
+      ),
+    );
+  }
+}
