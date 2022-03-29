63e7221f54c0159f453b246d6d55e0dd1fbaec68
Alessandro Stockman <alessandrostockman@gmail.com>
2022-03-29 20:10:19 +0200

    add picture url to user and advertisment

--- lib/models/advertisement.dart
+++ lib/models/advertisement.dart
@@ -1,34 +1,35 @@
 import 'package:envairo/models/user.dart';
 import 'package:flutter/material.dart';
 
-class Advertisement{
+class Advertisement {
+  static const String defaultImage =
+      "https://www.artemedialab.it/wp-content/uploads/2019/04/immagini-sfondo-1-700x400.jpg";
+
   final int id;
   final String title;
   final String description;
+  final String mainImage;
   final List<Image>? images;
   final DateTime? availabilityTime;
   final User seller;
   //Queue stuffs
 
+  Advertisement(
+      {required this.id,
+      required this.seller,
+      required this.title,
+      this.mainImage = Advertisement.defaultImage,
+      this.availabilityTime,
+      this.description = "",
+      this.images});
 
-  Advertisement({
-    required this.id,
-    required this.seller,
-    required this.title,
-    this.availabilityTime,
-    this.description = "",
-    this.images
-  });
-
-
-  factory Advertisement.fromJson(Map<String, dynamic> json){
+  factory Advertisement.fromJson(Map<String, dynamic> json) {
     return Advertisement(
         id: json["id"],
         seller: User.fromJson(json['user']),
         title: json['title'],
         description: json["description"],
         availabilityTime: json["availabilityTime"],
-        images: json['images']
-    );
+        images: json['images']);
   }
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,21 +1,30 @@
 import 'package:envairo/models/position.dart';
+import 'package:flutter/material.dart';
 
-class User{
+class User {
+  static const String defaultPicture =
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+
   final String email;
   final String username, name;
   final String? surname;
+  final String picture;
+  final double? reputation;
+  final int? reviewCount;
 
   final Position? position;
 
-  const User({
-    required this.email,
-    required this.username,
-    required this.name,
-    this.surname,
-    this.position
-  });
+  const User(
+      {required this.email,
+      required this.username,
+      required this.name,
+      this.picture = User.defaultPicture,
+      this.surname,
+      this.position,
+      this.reputation,
+      this.reviewCount});
 
-  factory User.fromJson(Map<String, dynamic> json){
+  factory User.fromJson(Map<String, dynamic> json) {
     return User(
       email: json['email'],
       username: json['username'],
