0f4d2de5caad5ee90a2a85faea990b5b6a43b55f
younik <omar.younis98@gmail.com>
2022-03-30 22:47:04 +0200

    add maps

--- lib/view/pages/search_page.dart
+++ lib/view/pages/search_page.dart
@@ -1,18 +1,55 @@
-import 'package:envairo/models/advertisement.dart';
-import 'package:envairo/view/pages/advertisement_grid.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    List<Advertisement> advs = Advertisement.generateFake(20, 5);
+  State<SearchPage> createState() => _SearchPageState();
+}
 
-    return Column(
+class _SearchPageState extends State<SearchPage> {
+  late Completer<GoogleMapController> controller1;
+
+  final Set<Marker> _markers = {
+    const Marker(position: LatLng(40.6508946,16.1456264), markerId: MarkerId("test9")),
+    const Marker(position: LatLng(42.0481283,14.709342), markerId: MarkerId("test8")),
+    const Marker(position: LatLng(45.155062, 11.663693), markerId: MarkerId("test7")),
+    const Marker(position: LatLng(44.4897103,11.3131531), markerId: MarkerId("test6")),
+    const Marker(position: LatLng(40.1484675,9.0284305), markerId: MarkerId("test5")),
+    const Marker(position: LatLng(37.7486814,14.8072024), markerId: MarkerId("test4")),
+    const Marker(position: LatLng(45.1055772,9.7201355), markerId: MarkerId("test3")),
+    const Marker(position: LatLng(42.1311128,11.9113219), markerId: MarkerId("test2")),
+    const Marker(position: LatLng(41.9102415,12.3959112), markerId: MarkerId("test"))
+  };
+
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      controller1.complete(controller);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
         children: [
+          GoogleMap(
+            markers: _markers,
+            mapType: MapType.normal,
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(42.1311128,11.9113219),
+              zoom: 5,
+            ),
+            onMapCreated: _onMapCreated,
+            zoomGesturesEnabled: true,
+            myLocationEnabled: true,
+            compassEnabled: true,
+            myLocationButtonEnabled: false,
+          ),
+
           Padding(
             padding: EdgeInsets.only(top: 75.h, left: 25.w, right: 25.w),
             child: TextField(
@@ -29,9 +66,8 @@ class SearchPage extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: AdvertisementGrid(advs: advs),
-          ),
-        ]);
+        ]
+
+    );
   }
 }
